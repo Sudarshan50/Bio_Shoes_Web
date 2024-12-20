793e5a1295295cf2393b9d891e7500999debfca7
Sudarshan@050 <oraonsudarshan@gmail.com>
2024-12-21 01:33:06 +0530

    Add frontend assets including various icons and images; update .gitignore to include assets directory...

---
+++ client/src/assets/frontend_assets/assets.js
@@ -0,0 +1,294 @@
+import logo from "./logo.png";
+import hero_img from "./hero_img.png";
+import cart_icon from "./cart_icon.png";
+import bin_icon from "./bin_icon.png";
+import dropdown_icon from "./dropdown_icon.png";
+import exchange_icon from "./exchange_icon.png";
+import profile_icon from "./profile_icon.png";
+import quality_icon from "./quality_icon.png";
+import search_icon from "./search_icon.png";
+import star_dull_icon from "./star_dull_icon.png";
+import star_icon from "./star_icon.png";
+import support_img from "./support_img.png";
+import menu_icon from "./menu_icon.png";
+import about_img from "./about_img.png";
+import contact_img from "./contact_img.png";
+import razorpay_logo from "./razorpay_logo.png";
+import stripe_logo from "./stripe_logo.png";
+import cross_icon from "./cross_icon.png";
+import hero from "./hero.jpg";
+
+import p1 from "../gfoot_assets/s1.png";
+import p2 from "../gfoot_assets/s3.png";
+import p3 from "../gfoot_assets/s5.jpg";
+import p4 from "../gfoot_assets/s8.jpg";
+import p5 from "../gfoot_assets/s6.jpg";
+import p6 from "../gfoot_assets/s9.jpg";
+import p7 from "../gfoot_assets/s7.jpg";
+import p8 from "../gfoot_assets/s11.jpg";
+import p9 from "../gfoot_assets/s12.jpg";
+import p10 from "../gfoot_assets/s13.jpg";
+import p11 from "../gfoot_assets/s14.jpg";
+import p12 from "../gfoot_assets/s17.jpg";
+import p13 from "../gfoot_assets/s18.jpg";
+import p14 from "../gfoot_assets/s19.jpg";
+import p15 from "../gfoot_assets/s20.png";
+import p16 from "../gfoot_assets/s22.png";
+import p17 from "../gfoot_assets/s23.jpg";
+import p18 from "../gfoot_assets/s25.jpg";
+import p19 from "../gfoot_assets/s26.jpg";
+import p20 from "../gfoot_assets/s28.jpg";
+
+export const assets = {
+  logo,
+  hero_img,
+  cart_icon,
+  dropdown_icon,
+  exchange_icon,
+  profile_icon,
+  quality_icon,
+  search_icon,
+  star_dull_icon,
+  star_icon,
+  bin_icon,
+  support_img,
+  menu_icon,
+  about_img,
+  contact_img,
+  razorpay_logo,
+  stripe_logo,
+  cross_icon,
+  hero,
+};
+
+export const products = [
+  {
+    _id: "4d5e6f",
+    name: "Men's Suede Leather Sneakers with Thick Sole",
+    description:
+      "Casual sneakers made with suede leather and featuring a thick, comfortable sole.",
+    price: 2000,
+    category: "Men",
+    subCategory: "Casual Shoes",
+    sizes: [7, 8, 9, 10, 11],
+    image: [p2],
+  },
+  {
+    _id: "7g8h9i",
+    name: "Men's Perforated Low-Top Sneakers with Contrast Toe",
+    description:
+      "Casual sneakers with perforated detailing and a contrasting toe.",
+    price: 1400,
+    category: "Men",
+    subCategory: "Casual Shoes",
+    sizes: [7, 8, 9, 10, 11],
+    image: [p3],
+  },
+  {
+    _id: "0j1k2l",
+    name: "Unisex Multicolor Chunky Sneakers",
+    description:
+      "High-top sneakers with a chunky sole and a mix of colors and textures.",
+    price: 2000,
+    category: "Unisex",
+    subCategory: "Chunky Sneakers",
+    sizes: [8, 9, 10],
+    image: [p4],
+  },
+  {
+    _id: "3m4n5o",
+    name: "Unisex Low-Top Sneakers with Contrast Toe and Perforated Detailing",
+    description:
+      "Casual sneakers with a contrasting toe and perforated detailing.",
+    price: 1200,
+    category: "Unisex",
+    subCategory: "Casual Shoes",
+    sizes: [6, 7, 8, 9],
+    image: [p5],
+  },
+  {
+    _id: "6p7q8r",
+    name: "Unisex High-Top Sneakers with Chunky Sole",
+    description:
+      "Stylish high-top sneakers featuring a chunky, contrasting sole.",
+    price: 2500,
+    category: "Unisex",
+    subCategory: "Chunky Sneakers",
+    sizes: [7, 8, 9, 10, 11],
+    image: [p6],
+  },
+  {
+    _id: "9s0t1u",
+    name: "Men's Two-Tone Low-Top Sneakers",
+    description:
+      "Casual sneakers with a two-tone design and perforated detailing.",
+    price: 1400,
+    category: "Men",
+    subCategory: "Casual Shoes",
+    sizes: [6, 7, 8, 9, 10],
+    image: [p7],
+  },
+  {
+    _id: "2v3w4x",
+    name: "Men's Canvas Boat Shoes with White Sole",
+    description:
+      "Casual boat shoes made with canvas and featuring a white rubber sole.",
+    price: 1800,
+    category: "Men",
+    subCategory: "Casual Shoes",
+    sizes: [6, 7, 8, 9, 10],
+    image: [p8],
+  },
+  {
+    _id: "5y6z7a",
+    name: "Red and Black Chunky Sneakers",
+    description:
+      "Bold sneakers with a chunky sole and a contrasting red and black color scheme.",
+    price: 2200,
+    category: "Unisex",
+    subCategory: "Chunky Sneakers",
+    sizes: [6, 7, 8, 9, 10, 11],
+    image: [p9],
+  },
+  {
+    _id: "8b9c0d",
+    name: "Pastel Running Sneakers",
+    description:
+      "Lightweight sneakers with a mesh and suede design in pastel colors.",
+    price: 1900,
+    category: "Unisex",
+    subCategory: "Running Shoes",
+    sizes: [6, 7, 8, 9, 10],
+    image: [p10],
+  },
+  {
+    _id: "1e2f3g",
+    name: "Floral Print Sneakers",
+    description: "Casual sneakers featuring a delicate floral print design.",
+    price: 1500,
+    category: "Women",
+    subCategory: "Casual Shoes",
+    sizes: [5, 6, 7, 8, 9],
+    image: [p11],
+
+  },
+  {
+    _id: "4h5i6j",
+    name: "Leather Loafers with Gold Buckle",
+    description:
+      "Classic loafers crafted from leather with a stylish gold buckle detail.",
+    price: 2400,
+    category: "Men",
+    subCategory: "Casual Shoes",
+    sizes: [5, 6, 7, 8, 9],
+    image: [p12],
+    bestseller: true,
+
+  },
+  {
+    _id: "7k8l9m",
+    name: "Tan Leather Tassel Loafers with Metal Bit",
+    description:
+      "Classic loafers crafted from tan leather with a stylish metal bit and tassel detail.",
+    price: 2400,
+    category: "Men",
+    subCategory: "Casual Shoes",
+    sizes: [6, 7, 8, 9, 10],
+    image: [p13],
+    bestseller: true,
+
+  },
+  {
+    _id: "1a2b3c",
+    name: "Men's Breathable Running and Wear-Resistant Sports Shoes",
+    description:
+      "Breathable and durable shoes designed for running and other sports activities.",
+    price: 1500,
+    category: "Men",
+    subCategory: "Sports Shoes",
+    sizes: [8, 9, 10, 11, 12],
+    image: [p1],
+
+  },
+  {
+    _id: "0n1o2p",
+    name: "Black Leather Combat Boots",
+    description: "Sturdy and stylish combat boots crafted from black leather.",
+    price: 2500,
+    category: "Men",
+    subCategory: "Boots",
+    sizes: [6, 7, 8, 9, 10],
+    image: [p14],
+    bestseller: true,
+  },
+  {
+    _id: "3q4r5s",
+    name: "Purple Knitted Sneakers",
+    description:
+      "Lightweight and breathable sneakers with a knitted upper in a vibrant purple color.",
+    price: 1900,
+    category: "Unisex",
+    subCategory: "Casual Shoes",
+    sizes: [6, 7, 8, 9, 10],
+    image: [p15],
+  },
+  {
+    _id: "6t7u8v",
+    name: "Neon Red Perforated Sneakers",
+    description:
+      "Bold sneakers in a vibrant neon red with a perforated suede upper.",
+    price: 1900,
+    category: "Unisex",
+    subCategory: "Casual Shoes",
+    sizes: [6, 7, 8, 9, 10],
+    image: [p16],
+  },
+  {
+    _id: "9w0x1y",
+    name: "Textured Slip-on Sneakers",
+    description:
+      "Comfortable slip-on sneakers with a textured fabric upper and a white sole.",
+    price: 1600,
+    category: "Unisex",
+    subCategory: "Slip-on",
+    sizes: [6, 7, 8, 9, 10],
+    image: [p17],
+    bestseller: true,
+  },
+  {
+    _id: "2z3a4b",
+    name: "Pink Mesh Sneakers",
+    description:
+      "Lightweight sneakers with a breathable mesh upper in a soft pink color.",
+    price: 1800,
+    category: "Women",
+    subCategory: "Casual Shoes",
+    sizes: [5, 6, 7, 8, 9],
+    image: [p18],
+    bestseller: true,
+  },
+  {
+    _id: "5c6d7e",
+    name: "Green Textured Slip-on Sneakers",
+    description:
+      "Casual slip-on sneakers with a textured woven fabric upper in a light green color.",
+    price: 1700,
+    category: "Unisex",
+    subCategory: "Slip-on",
+    sizes: [5, 6, 7, 8, 9],
+    image: [p19],
+    bestseller: true,
+  },
+  {
+    _id: "8f9g0h",
+    name: "White Leather Sneakers with Pink Accents",
+    description:
+      "Classic low-top sneakers with a white leather upper and subtle pink accents.",
+    price: 1800,
+    category: "Women",
+    subCategory: "Casual Shoes",
+    sizes: [6, 7, 8, 9, 10],
+    image: [p20],
+    bestseller: true,
+  },
+];
